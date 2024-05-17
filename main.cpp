178dd8ae928af57d11bffc8df837fe561eb7a388
QmN23 <jokerbeagle@mail.ru>
2024-05-17 23:04:27 +0200

    lab5

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <Circle.hpp>
+#include <Game.hpp>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    mt::Game game(1000, 600, "Game");
+    game.Setup(10);
+
+    game.LifeCycle();
+
+    return 0;
+}
